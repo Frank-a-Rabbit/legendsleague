d24d7551098b8b314968c5674b17d0f547895ed2
Justin f Booth <p0rkchopexp@outlook.com>
2023-07-26 21:16:20 -0700

    login page updates

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react"
-import { UserContext, userCtx } from "../context/UserContext"
+import React, { useContext, useState, useEffect } from "react"
+import { User, UserContext } from "../context/UserContext"
 import { firebase, FacebookAuthProvider, GoogleAuthProvider, signInWithPopup } from "../utils/Firebase"
 import { databaseConnection } from "../utils/Firebase"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -7,82 +7,100 @@ import { faGoogle, faFacebook } from "@fortawesome/free-brands-svg-icons"
 import styles from "../styles/Nav.module.css"
 import { useRouter } from "next/router"
 
-type loginProps = {
-    navItem : string
-}
-
-const Login = ({ navItem }: loginProps) => {
-    const { currentUser, setUser } = useContext(UserContext) as userCtx
+const Login = () => {
+    const { currentUser, setUser } = useContext(UserContext)
+    const [newClassName, setNewClassName] = useState<string>("")
     const router = useRouter()
     const siginWithProvider = (provider : any) => {
         switch(provider) {
             case "go":
                 let googleService = GoogleAuthProvider
-                firebase.auth().signInWithPopup(googleService).then(result => {
-                    databaseConnection.collection("users").doc(result.user.uid).get().then(doc => {
-                        if (doc.exists) {
-                            setUser(result.user)
-                            router.push("/")
-                        } else {
-                            databaseConnection.collection("users").doc(result.user.uid).set({
-                                email : result.user.email,
-                                name : result.user.displayName,
-                                photo : result.user.photoURL,
-                                wins : 0,
-                                losses : 0
-                            }).then(() => {
-                                setUser(result.user)
-                                router.push("/")
-                            })
+                firebase.auth().signInWithPopup(googleService).then((result) => {
+                    if (result.user) {
+                        const { user, uid, photoURL } = result.user
+                        const userData: User = {
+                            user,
+                            uid,
+                            photoURL: photoURL || null,
                         }
-                    })
+                        databaseConnection.collection("users").doc(result.user.uid).get().then(doc => {
+                            if (doc.exists) {
+                                setUser(userData)
+                                router.push("/play")
+                            } else {
+                                databaseConnection.collection("users").doc(userData.uid).set({
+                                    email : userData.user?.email,
+                                    name : userData.user?.displayName,
+                                    photo : userData.photoURL,
+                                    wins : 0,
+                                    losses : 0
+                                }).then(() => {
+                                    setUser(userData)
+                                    router.push("/play")
+                                })
+                            }
+                        })
+                    }
                 })
             break
             case "fb":
                 let fbService = FacebookAuthProvider
                 firebase.auth().signInWithPopup(fbService).then(result => {
-                    databaseConnection.collection("users").doc(result.user.uid).get().then(doc => {
-                        if (doc.exists) {
-                            setUser(result.user)
-                            router.push("/")
-                        } else {
-                            databaseConnection.collection("users").doc(result.user.uid).set({
-                                email : result.user.email,
-                                name : result.user.displayName,
-                                photo : result.user.photoURL,
-                                wins : 0,
-                                losses : 0
-                            }).then(() => {
-                                setUser(result.user)
-                                router.push("/")
-                            })
+                    if (result.user) {
+                        const { user, uid, photoURL } = result.user
+                        const userData: User = {
+                            user,
+                            uid,
+                            photoURL: photoURL || null,
                         }
-                    })
+                        databaseConnection.collection("users").doc(userData.uid).get().then(doc => {
+                            if (doc.exists) {
+                                setUser(userData)
+                                router.push("/play")
+                            } else {
+                                databaseConnection.collection("users").doc(userData.uid).set({
+                                    email : userData.user?.email,
+                                    name : userData.user?.displayName,
+                                    photo : userData.photoURL,
+                                    wins : 0,
+                                    losses : 0
+                                }).then(() => {
+                                    setUser(userData)
+                                    router.push("/play")
+                                })
+                            }
+                        })
+                    }
                 })
             break
         }
     }
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setNewClassName(styles.active)
+        }, 700)
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [])
     return (
         <section className={styles.loginCont}>
-            <div className={styles.infoCont}>
-                <p>Choose from one of the providers below to create an account or login to an existing account.</p>
+            <div className={`${styles.outer} ${newClassName}`}>
+                <div className={styles.inner}>
+                    <button className={styles.close}>Close</button>
+                    <div className={styles.infoCont}>
+                        <p>Choose from one of the providers below to create an account or login to an existing account.</p>
+                    </div>
+                    <nav className={styles.navCont}>
+                        <ul>
+                            <li onClick={() => siginWithProvider("fb")}><FontAwesomeIcon icon={faFacebook} />Facebook</li>
+                            <li onClick={() => siginWithProvider("go")}><FontAwesomeIcon icon={faGoogle} />Google</li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            <nav className={styles.navCont}>
-                <ul>
-                    <li onClick={() => siginWithProvider("fb")}><FontAwesomeIcon icon={faFacebook} />Facebook</li>
-                    <li onClick={() => siginWithProvider("go")}><FontAwesomeIcon icon={faGoogle} />Google</li>
-                </ul>
-            </nav>
         </section>
     )
-}
-
-export async function getStaticProps() {
-    return {
-        props : {
-            navItem : "login"
-        }
-    }
 }
 
 export default Login
